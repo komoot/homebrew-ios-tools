91dd18130ee99b99a5b07e570eb26b1c9e014fc7
Christian Huck <c.huck@me.com>
2022-10-01 21:45:58 +0200

    Update bob.rb

--- bob.rb
+++ bob.rb
@@ -1,11 +1,32 @@
+
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Bob < Formula
-  desc "Module manager for the komoot iOS team"
-  homepage "https://github.com/your/project"
-  url "https://github.com/komoot/bob-the-module-maker"
+  desc ""
+  homepage ""
+  license ""
+  head "git@github.com:komoot/bob-the-module-maker.git"
+
+  # depends_on "cmake" => :build
+
   def install
     system "swift", "build",
         "--configuration", "release",
         "--disable-sandbox"
     bin.install '.build/release/bob'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bob`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
