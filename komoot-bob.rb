497e2cdf3c9081787c40c0a419e98610f74cceff
Christian Huck <c.huck@me.com>
2022-10-01 23:51:25 +0200

    Update komoot-bob.rb

--- komoot-bob.rb
+++ komoot-bob.rb
@@ -2,7 +2,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Komoot-Bob < Formula
+class Bob < Formula
   desc ""
   homepage ""
   license ""
