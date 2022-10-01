94035d3875c324cec5c26f79bba44f5d0662e342
Christian Huck <c.huck@me.com>
2022-10-01 23:53:47 +0200

    Update and rename komoot_bob.rb to komoot-bob.rb

--- komoot-bob.rb
+++ komoot-bob.rb
@@ -2,7 +2,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Komoot_Bob < Formula
+class KomootBob < Formula
   desc ""
   homepage ""
   license ""
